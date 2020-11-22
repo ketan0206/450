da0d5f61b0c74ed783a9e816158caa9777f04060
ketan0206 <ketangoel2020>
2020-11-22 21:45:34 +0530

    recursive solution is nice.

---
+++ 22Nov20 Reverse LinkedList.java
@@ -0,0 +1,75 @@
+// https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int value;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class ReverseLL
+{
+    // This function should reverse linked list and return
+    // head of the modified linked list.
+    
+    
+   /* Below solution by ketan using stacks.
+    Node reverseList(Node head)
+    {
+        Stack<Node> stack = new Stack<>();
+        Node temp = head;
+        while(temp!=null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        
+        head = stack.pop();
+        Node revTemp = head;
+        while(!stack.isEmpty()) {
+            revTemp.next = stack.pop();
+            revTemp = revTemp.next;
+        }
+        revTemp.next=null;
+        
+        return head;
+    }*/
+    
+    /* Below solution by ketan using iteration.
+    Node reverseList(Node head)
+    {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while(curr!=null) {
+            if (curr.next!=null) {
+                next = curr.next;
+            }
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            head = curr;
+            curr = next;
+        }
+        return head;
+    }*/
+    
+    // Below solution by ketan using recursion..
+    Node reverseList(Node head)
+    {
+        if (head==null || head.next==null) return head;
+        Node temp = reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return temp;
+    }
+    
+}
