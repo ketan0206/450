b038b26f6338442baf119a2b61c97e5805ef63d1
ketan0206 <ketangoel2020>
2020-12-13 01:36:09 +0530

    Elegant solution for iterative version for preorder traversal BT.

--- 13Dec20 Preorder Traversal BT.java
+++ 13Dec20 Preorder Traversal BT.java
@@ -1,16 +1,25 @@
 // https://leetcode.com/problems/binary-tree-preorder-traversal/submissions/
 
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        traverse(root, list);
-        return list;
-    }
-    
-    private void traverse (TreeNode root, List<Integer> list) {
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> list = new ArrayList<>();
+        //traverse(root, list);
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+		while (!stack.isEmpty()) {
+			TreeNode node = stack.pop();
+			if (node != null) {
+				list.add(node.val);
+				stack.push(node.right);
+				stack.push(node.left);
+			}
+		}
+		return list;
+	}
+    /*private void traverse (TreeNode root, List<Integer> list) {
         if (root==null) return;
         list.add(root.val);
         traverse(root.left, list);
         traverse(root.right, list);
-    }
+    }*/
 }
