4b02fe2a367b4046cc746b8b7554f8cb309e8dd0
ketan0206 <ketangoel2020>
2020-12-13 09:35:24 +0530

    Iterative Solution. Explore more.

--- 13Dec20 PostOrder Traversal BT.java
+++ 13Dec20 PostOrder Traversal BT.java
@@ -3,14 +3,42 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        traverse(root, list);
+        //traverse(root, list);
+        Stack<TreeNode> s1 = new Stack<>(); 
+        Stack<TreeNode> s2 = new Stack<>(); 
+  
+        if (root == null) 
+            return list; 
+  
+        // push root to first stack 
+        s1.push(root); 
+  
+        // Run while first stack is not empty 
+        while (!s1.isEmpty()) { 
+            // Pop an item from s1 and push it to s2 
+            TreeNode temp = s1.pop(); 
+            s2.push(temp); 
+  
+            // Push left and right children of 
+            // removed item to s1 
+            if (temp.left != null) 
+                s1.push(temp.left); 
+            if (temp.right != null) 
+                s1.push(temp.right); 
+        } 
+  
+        // Print all elements of second stack 
+        while (!s2.isEmpty()) { 
+            TreeNode temp = s2.pop(); 
+            list.add(temp.val);
+        } 
         return list;
     }
     
-    private void traverse(TreeNode root, List<Integer> list) {
+    /*private void traverse(TreeNode root, List<Integer> list) {
         if (root==null) return;
         traverse(root.left, list);
         traverse(root.right, list);
         list.add(root.val);
-    }
+    }*/
 }
